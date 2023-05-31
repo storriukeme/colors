f68a5caf5011e3db57fc7e7a320ddba88678d150
storriukeme <ukeme@storri.ai>
2023-05-31 16:39:39 +0100

    adding files

--- colors/color.py
+++ colors/color.py
@@ -1,7 +1,12 @@
 import numpy as np
 from settings import logger
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_color():
-    logger.info(f"sub module requirements.txt works {np.array([2, 3, 4])}")
-    
+    logger.info(f"sub module requirements.txt works {type(np.array([2, 3, 4]))}")
+    logger.info(f"the secret is {os.environ['MYSECRET']} ")
     return "Red"
+
